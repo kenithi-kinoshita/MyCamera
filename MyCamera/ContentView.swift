43bb29d3ccae00d52cbb65781a2e7704acf8a6e7
k-kinoshita <kinoken5939@gmail.com>
2021-10-22 15:21:36 +0900

    カメラボタン実装

--- MyCamera/ContentView.swift
+++ MyCamera/ContentView.swift
@@ -9,10 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+        // 縦方向にレイアウト
+        VStack {
+            Spacer()
+            // 「カメラを起動する」ボタン
+            Button(action: {
+                // ボタンをタップした時のアクション
+                // カメラが利用可能かチェック
+                if UIImagePickerController.isSourceTypeAvailable(.camera){
+                    print("カメラは利用できます")
+                } else {
+                    print("カメラは利用できません")
+                }
+
+            }) {
+                Text("カメラを起動する")
+                    // 横いっぱい
+                    .frame(maxWidth: .infinity)
+                    // 高さ50ポイントを指定
+                    .frame(height: 50)
+                    // 文字列をセンタリング指定
+                    .multilineTextAlignment(.center)
+                    // 背景を青色に指定
+                    .background(Color.blue)
+                    // 文字色を白色に指定
+                    .foregroundColor(Color.white)
+            } // Button end
+        } // VStack end
+    } // body end
+} // ContentView end
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
