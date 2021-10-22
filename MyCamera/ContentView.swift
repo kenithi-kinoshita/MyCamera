b88ec38e055347722c0355d3668cf762f2b5001a
k-kinoshita <kinoken5939@gmail.com>
2021-10-22 18:27:20 +0900

    カメラの呼び出しと反映実装

--- MyCamera/ContentView.swift
+++ MyCamera/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    // 撮影する写真を保持する状態変数
+    @State var captureImage: UIImage? = nil
+    // 撮影画面のsheet
+    @State var isShowSheet = false
+    
     var body: some View {
         // 縦方向にレイアウト
         VStack {
+            Spacer()
+            // 撮影した写真がある時
+            if let unwrapCaptureImage = captureImage {
+                // 撮影写真を表示
+                Image(uiImage: unwrapCaptureImage)
+                    // リサイズ
+                    .resizable()
+                    // アスペクト比を維持して画面内に
+                    .aspectRatio(contentMode: .fit)
+            }
             Spacer()
             // 「カメラを起動する」ボタン
             Button(action: {
@@ -18,6 +33,8 @@ struct ContentView: View {
                 // カメラが利用可能かチェック
                 if UIImagePickerController.isSourceTypeAvailable(.camera){
                     print("カメラは利用できます")
+                    // カメラが使えるなら、isShowSheetをtrue
+                    isShowSheet = true
                 } else {
                     print("カメラは利用できません")
                 }
@@ -34,7 +51,15 @@ struct ContentView: View {
                     .background(Color.blue)
                     // 文字色を白色に指定
                     .foregroundColor(Color.white)
-            } // Button end
+            } // カメラ起動Button end
+            .padding()
+            // sheetを表示
+            // isPresentedで指定した状態変数がtrueのとき実行
+            .sheet(isPresented: $isShowSheet) {
+                // UIImagePickerControllerをsheetを表示
+                ImagePickerView(isShowSheet: $isShowSheet,
+                                captureImage: $captureImage)
+            }// カメラ起動Button sheet end
         } // VStack end
     } // body end
 } // ContentView end
