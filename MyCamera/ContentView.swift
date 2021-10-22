fffb9ca4d69a5500a8072ee1ed6016e7971f0536
k-kinoshita <kinoken5939@gmail.com>
2021-10-22 10:36:29 +0900

    マイカメラ新規作成

---
+++ MyCamera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyCamera
+//
+//  Created by 木下健一 on 2021/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
